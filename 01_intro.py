bd92ee4d0dbf6b91072de83d3af85c24908f2de4
Dasha <konopelko@step.rivne>
2024-06-05 15:03:14 +0300

    intro PythonCore

---
+++ 01_intro.py
@@ -0,0 +1,10 @@
+# print("\tHello World!!!","HTML","CSS","C++","C#",sep="\n",end="   --- ")
+# print("\t \t Hyper \"Text\" Markup \\aLa\'nguage")
+# print("\t \t Hyper \"Text\" Markup \\aLa\'nguage")
+# print("\t \t Hyper \"Text\" Markup \\aLa\'nguage")
+# print("\t \t Hyper \"Text\" Markup \\aLa\'\bnguage")
+# print("Hello".upper())
+# print("Hello".lower())
+# print("\033[31m\033[1m hello".capitalize())
+
+
