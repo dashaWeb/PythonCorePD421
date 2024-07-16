362d93c6c73ba1a66d7bbfc23707a5fdc384c739
Dasha <konopelko@step.rivne>
2024-07-16 15:46:48 +0300

    recursive

---
+++ 15_function_recursive.py
@@ -0,0 +1,44 @@
+'''
+!5 = 5 * !4
+!4 = 4 * !3
+!3 = 3 * !2
+!2 = 2 * !1
+!1 = 1
+!0 = 1 
+'''
+
+
+def factorial(number):
+    if number == 0 or number == 1:
+        return 1
+    return number * factorial(number-1)
+
+
+print(factorial(5))
+
+
+# 1 - 10 (sum)
+# 1 + 2-10
+# 2 + 3 - 10
+# 3 + 4 - 10
+def sumRange(a, b):
+    if a == b:
+        return a
+    return a + sumRange(a+1, b)
+
+
+print(sumRange(1, 10))
+
+# 2^4 --> 2 * 2^3
+# 2^3 --> 2 * 2^2
+# 2^2 --> 2 * 2^1
+# 2^1 --> 2
+
+def powRec(numb, pow):
+    if pow == 1:
+        return numb
+    return numb * powRec(numb, pow -1)
+
+
+
+print(powRec(2, 4))
